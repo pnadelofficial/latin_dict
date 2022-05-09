bbd8b1c02d8599c1ad39f0f94467f9a719b43033
pnadelofficial <peter.m.nadel@gmail.com>
2022-05-09 13:27:16 -0400

    initial commit

---
+++ dashboard.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import streamlit as st
+import re
+import pickle
+from util import *
+
+st.title('Latin Verb Search')
+st.markdown('Enhancing the *Lewis and Short* Latin dictionary with ITTB Latin treebanks')
+
+las_df = pd.read_csv('las_csv.csv').rename(columns={'Unnamed: 0':'lemma','0':'entry'})
+
+with open('lemmas_nonums.pickle', 'rb') as f:
+  verb_lemmas_fromdict = pickle.load(f)
+
+stats = open('UD_Latin-ITTB/stats.xml')
+stat_xml = BeautifulSoup(stats)
+deprel_list = [] 
+for dep in stat_xml.find_all('deps')[0].find_all('dep'):
+  deprel_list.append(dep.attrs['name'])
+
+st.markdown("Search for a verb.")
+verb_search = st.text_input('Give first person, singular, indicative form of any verb.')
+verb_search = verb_search.lower()
+if verb_search:
+  st.write(verb_search)
+  st.write(generateStatDicts(verb_search))
+  st.write('Guide to [deprel tags](https://universaldependencies.org/treebanks/la_ittb/index.html#relations)')
+
+  sub_df = las_df.applymap(lambda x: re.findall(f'(^{verb_search}\d)\|(^{verb_search})',x))
+  sub_lemma = las_df['lemma'].apply(lambda x: re.findall(f'(^{verb_search})',x))
+  sub_idx = sub_lemma.loc[sub_lemma.str.len() > 0].index
+  sub_lemma_d = las_df['lemma'].apply(lambda x: re.findall(f'(^{verb_search}\d)',x))
+  sub_d_idx = sub_lemma.loc[sub_lemma.str.len() > 0].index
+  final = las_df.iloc[sub_idx]
+  if len(final) == 0:
+    final = las_df.iloc[sub_d_idx]
+
+  st.write(final.loc[final['lemma'].apply(lambda x: lemmaFilter(x, verb_search))]['entry'].iloc[0])
---
+++ util.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import pickle
+import ast
+from stanza.utils.conll import CoNLL
+
+dicts = CoNLL.conll2doc('UD_Latin-ITTB/la_ittb-ud-train.conllu')
+trees = {}
+for id, sent in enumerate(dicts.sentences):
+  trees[id] = sent
+trees_df = pd.DataFrame.from_dict(trees, orient='index')
+
+intersect_df = pd.read_csv('intersect_csv.csv')
+intersect_df['0'] = intersect_df['0'].apply(lambda x: x.replace(' ', ', ')) 
+intersect_df['0'] = intersect_df['0'].apply(ast.literal_eval)
+
+with open('deprel_list.pickle','rb') as f:
+  deprel_list = pickle.load(f)
+
+def deprelDict(sentence, search):
+  deprel_dict = {}
+  for dep in sentence.dependencies:
+    if dep[0].lemma == search or dep[2].lemma == search:
+      for deprel in deprel_list:
+        if (deprel == dep[1]) and (dep[1] != 'punct'):
+          if deprel in deprel_dict:
+            deprel_dict[deprel] += 1
+          else:
+            deprel_dict[deprel] = 1
+  return deprel_dict
+
+def deprelDictHeaderFooter(sentence, search):
+  header_dict = {}
+  footer_dict = {}
+  for dep in sentence.dependencies:
+    if dep[0].lemma == search:
+      for deprel in deprel_list:
+        if (deprel == dep[1]) and (dep[1] != 'punct'):
+          if deprel in header_dict:
+            header_dict[deprel] += 1
+          else:
+            header_dict[deprel] = 1
+    elif dep[2].lemma == search:
+      for deprel in deprel_list:
+        if (deprel == dep[1]) and (dep[1] != 'punct'):
+          if deprel in footer_dict:
+            footer_dict[deprel] += 1
+          else:
+            footer_dict[deprel] = 1
+  return header_dict, footer_dict
+
+def generateStatDicts(search):
+  from collections import Counter
+
+  sub_df = trees_df.iloc[intersect_df.explode('0').loc[intersect_df.explode('0')['0'] == search]['index'].to_list()]
+  total = sub_df[0].apply(lambda x: deprelDict(x, search))
+  header = sub_df[0].apply(lambda x: deprelDictHeaderFooter(x, search)).apply(lambda x: x[0])
+  footer = sub_df[0].apply(lambda x: deprelDictHeaderFooter(x, search)).apply(lambda x: x[1])
+
+  counts = sum(map(Counter, total), Counter())
+  res = pd.DataFrame.from_dict(counts, orient='index')
+  res[res.columns[0]] = (res[res.columns[0]]/res[res.columns[0]].sum())*100
+
+  h_counts = sum(map(Counter, header), Counter())
+  h_res = pd.DataFrame.from_dict(h_counts, orient='index')
+  h_res[h_res.columns[0]] = (h_res[h_res.columns[0]]/h_res[h_res.columns[0]].sum())*100
+
+  f_counts = sum(map(Counter, footer), Counter())
+  f_res = pd.DataFrame.from_dict(f_counts, orient='index')
+  f_res[f_res.columns[0]] = (f_res[f_res.columns[0]]/f_res[f_res.columns[0]].sum())*100
+
+  res['asheader'] = h_res[h_res.columns[0]]
+  res['asfooter'] = f_res[f_res.columns[0]]
+  res = res.fillna(0)
+  return res.rename(columns={0:'Total', 'asheader':f'{search} as a head', 'asfooter':f'{search} as a modifier'})
+
+def lemmaFilter(lemma, verb_search):
+  return lemma.split(verb_search)[1].isnumeric() or lemma.split(verb_search)[1] == ''
